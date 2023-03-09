be771fe4e0b9e56270ba8a9380f2aa3bd1142ff2
KhangtndDev <khangtnd.dev@gmail.com>
2023-03-09 21:43:11 +0700

    add first code solidity

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+contract SimpleStorage {
+    bool hasFavoriteNumber = true;
+    uint256 favoriteNumber = 5;
+    string favoriteNumberInText = "Five";
+    int256 favoriteInt = -5;
+    address myAddress = 0xd97d604C521651921801Dbb0cAc54120Cf3Fb254;
+    bytes32 favoriteBytes = "cat";
+}
